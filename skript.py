7fb8fd4f547fbbbae1a2726019c51fdb5126fa9f
MartinBierbaumer <119934665+MartinBierbaumer@users.noreply.github.com>
2022-12-13 11:45:40 +0100

    TODO

--- skript.py
+++ skript.py
@@ -6,11 +6,14 @@ import os
 import argparse
 import json
 import re
+import logging
+
+#TODO logging, paths, match
 
 
 def getProjectEndpoint(host="localhost", port="3080", projectName=None):
     """returns the GNS-API projectEndpoint"""
-    
+
     endPoint = "http://" + host + ":" + port + "/v2/projects"
     r = requests.get(endPoint)
     for project in r.json():
@@ -18,15 +21,15 @@ def getProjectEndpoint(host="localhost", port="3080", projectName=None):
             projectID = project["project_id"]
     return endPoint + "/" + projectID
 
+
 def getDevices(projectEndpoint):
     """returns an iterator over all devices with the GNS-API-device information"""
-    
+
     r = requests.get(projectEndpoint + "/nodes")
     return r.json()
 
-def getKonfig(telnet, switch=True):
-    """retrieves the konfig and return it as a string"""
 
+def setMode(telnet):
     telnet.write(b"\n")
     telnet.write(b"\n")
     telnet.write(b"\n")
@@ -36,16 +39,21 @@ def getKonfig(telnet, switch=True):
     telnet.write(b"conf t\n")
     telnet.write(b"end\n")
     telnet.write(b"terminal length 0\n")
+
+
+def getRunning(telnet):
     telnet.write(b"show run\n")
     telnet.read_until(b" bytes", 2)
+    return telnet.read_until(b"!\r\nend", 30).decode('utf-8') + "\n"
 
-    running = telnet.read_until(b"!\r\nend", 30).decode('utf-8')
+
+def getVlan(telnet, switch):
+    vlans = []
 
     if (switch):
         telnet.write(b"show vlan brief\n")
         data = telnet.read_until(b"end sequence", 2).decode('utf-8')
-        pattern = '^(\\d+)[\\s]+([\\S]+)[\\s]+([\\S]+)'
-        vlans = []
+        pattern = r'^(\d+)[\s]+([\S]+)[\s]+([\S]+)'
 
         for line in data.split("\n"):
             matches = re.finditer(pattern, line)
@@ -58,20 +66,34 @@ def getKonfig(telnet, switch=True):
         print(data)
         print(vlans)
 
-    return running
+    return "\n".join([f"vlan {vlan[0]}\nname {vlan[1]}" for vlan in vlans]) + "\n"
+
+
+def getKonfig(telnet, switch=True):
+    """retrieves the konfig and return it as a string"""
+
+    setMode(telnet)
+    running = getRunning(telnet)
+    vlan = getVlan(telnet, switch)
+
+    return running + vlan
+
 
 sw_hashes = ['14b981002e40b660f2d7400401e04c14', '8f14b50083a14688dec2fc791706bb3e']
 ro_hashes = ['e7cb1bbd0c59280dd946feefa68fa270', '37c148ffa14a82f418a6e9c2b049fafe']
 
+
 def saveItem(vmhost, device, path):
     """writes the konfig to a file"""
-    
+
     with open(path, "w") as file:
-	    file.write(getKonfig(telnetlib.Telnet(vmhost, device["console"]), switch=device['properties']['hda_disk_image_md5sum'] in sw_hashes))
+        file.write(getKonfig(telnetlib.Telnet(vmhost, device["console"]),
+                             switch=device['properties']['hda_disk_image_md5sum'] in sw_hashes))
+
 
 def saveKonfig(vmhost, projectEndpoint, path):
     """creates multiple threads in order to open telnetconnections and save the configurations in parallel"""
-    
+
     try:
         os.mkdir(path)
     except:
@@ -83,15 +105,17 @@ def saveKonfig(vmhost, projectEndpoint, path):
         thread = threading.Thread(target=saveItem, args=(vmhost, device, path + "/" + device["name"] + ".skript"))
         thread.start()
         threads.append(thread)
-    
+
     for thread in threads:
         thread.join()
+
 
 def savePhysical(vmhost, projectEndpoint, path):
     """saves the physical config"""
 
     with open(path + "/konfig.konf", "w") as file:
         file.write(json.dumps(getDevices(projectEndpoint)))
+
 
 def save(vmhost, projectName, path):
     """saves a GNS-project"""
@@ -105,14 +129,11 @@ def save(vmhost, projectName, path):
     savePhysical(vmhost, projectEndpoint, path)
 
 
-
 def load(vmhost, projectName, path):
     with open(path, "r") as json_file:
         data = json.load(json_file)
     for device in data:
         print(device)
-
-
 
 
 parser = argparse.ArgumentParser(prog="GNScript", description="retrieves config-skripts from GNS")
